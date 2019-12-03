a5a6db13b15140f526406f8f46da90b1b2e6d24e
Gabriel Majeri <gabriel.majeri6@gmail.com>
2019-12-03 11:40:15 +0200

    Clasa Vector 2D

---
+++ prgc/primitive.py
@@ -0,0 +1,18 @@
+class Vector2D:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def __add__(self, other):
+        return Vector2D(self.x + other.x, self.y + other.y)
+
+    def __sub__(self, other):
+        return Vector2D(self.x - other.x, self.y - other.y)
+
+    def __eq__(self, other):
+        return self.x == other.x and self.y == other.y
+
+    def __repr__(self):
+        return f'({self.x}, {self.y})'
+
+Vector2D.ORIGIN = Vector2D(0, 0)
