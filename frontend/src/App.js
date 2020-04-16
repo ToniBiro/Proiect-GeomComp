afefa85bc1e256344c442c08b57e9c9284b22b85
Gabriel Majeri <gabriel.majeri6@gmail.com>
2020-04-16 18:29:00 +0300

    Start using React

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Stage, Layer, Rect, Text } from "react-konva";
+import "./App.css";
+
+export default function App() {
+  return (
+    <>
+      <h1>Intersecție de poligoane</h1>
+      <Stage width="360" height="360"></Stage>
+    </>
+  );
+}
