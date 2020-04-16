602cb023c76cd5867cdc019f70243684033f660d
Gabriel Majeri <gabriel.majeri6@gmail.com>
2020-04-16 18:29:00 +0300

    Start using React

--- frontend/index.js
+++
@@ -1,8 +0,0 @@
-const canvas = document.getElementById("canvas");
-
-const width = canvas.width;
-const height = canvas.height;
-
-const ctx = canvas.getContext("2d");
-
-ctx.fillRect(0, 0, width, height);
