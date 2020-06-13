cfae6a1c3257ad8763fe52c2633541a25b030e7f
ToniBiro <33375662+ToniBiro@users.noreply.github.com>
2020-06-13 19:55:11 +0300

    addressed bugs

--- backend/app.py
+++ backend/app.py
@@ -24,8 +24,8 @@ def compute_intersection():
     Input: (json format)2 lists of points.
     e.g.
     {
-            'rect1': [(0, 0), (1, 0), (1, 1), (0, 1)],
-            'rect2': [(0.5, -0.25), (1.25, -0.25), (1.25, 1.5), (0.5, 1.5)]
+            'polygon1': [(0, 0), (1, 0), (1, 1), (0, 1)],
+            'polygon2': [(0.5, -0.25), (1.25, -0.25), (1.25, 1.5), (0.5, 1.5)]
     }
 
     Returns: a list of points representing the polygon formed after the intersection.
@@ -39,26 +39,32 @@ def compute_intersection():
 
     """
     dcel = DCEL()
-    rect1_aux = []
-    rect2_aux = []
-    rect1 = []
-    rect2 = []
+    polygon1_aux = []
+    polygon2_aux = []
+    polygon1 = []
+    polygon2 = []
 
     # read json data from input
-    rect1_aux = request.get_json()['rect1']
-    for i, point in enumerate(rect1_aux):
-        rect1.append(Vector2D(point[0], point[1]))
-    rect1 = dcel.create_face_from_points(rect1)
+    polygon1_aux = request.get_json()['polygon1']
+    if polygon1_aux.is_json():
+        for i, point in enumerate(polygon1_aux):
+            polygon1.append(Vector2D(point[0], point[1]))
+        polygon1 = dcel.create_face_from_points(polygon1)
+    else:
+        return '\nWrong input.\nPlease pass the following format: {\n\t\'polygon1\': [(0, 0), (1, 0), ...],\n\t\'polygon2\': [(0.5, -0.25), (1.25, -0.25), ...]\n}'
 
-    rect2_aux = request.get_json()['rect2']
-    for i, point in enumerate(rect2_aux):
-        rect2.append(Vector2D(point[0], point[1]))
-    rect2 = dcel.create_face_from_points(rect2)
+    polygon2_aux = request.get_json()['polygon2']
+    if polygon2_aux.is_json():
+        for i, point in enumerate(polygon2_aux):
+            polygon2.append(Vector2D(point[0], point[1]))
+        polygon2 = dcel.create_face_from_points(polygon2)
+    else:
+        return '\nWrong input.\nPlease pass the following format: {\n\t\'polygon1\': [(0, 0), (1, 0), ...],\n\t\'polygon2\': [(0.5, -0.25), (1.25, -0.25), ...]\n}'
 
-    insect = intersect_polygons(dcel, rect1, rect2)
+    insect = intersect_polygons(dcel, polygon1, polygon2)
 
     # test prints - remove before deployment
-    print(rect1_aux)
+    print(polygon1_aux)
     print(len(insect))
 
 # Extract the result
@@ -102,14 +108,16 @@ def get_polygon_info():
 
     # read json data from input
     polygon = request.get_json()['polygon']
+    if polygon.is_json():
 
-    # compute the information
-    polygon_info['area'] = compute_area(polygon)
-    polygon_info['perimeter'] = compute_perimeter(polygon)
-    polygon_info['type'] = define_polygon_type(polygon)
+        # compute the information
+        polygon_info['area'] = compute_area(polygon)
+        polygon_info['perimeter'] = compute_perimeter(polygon)
+        polygon_info['type'] = define_polygon_type(polygon)
 
-    return jsonify(polygon_info)
-
+        return jsonify(polygon_info)
+    else:
+        return '\nWrong input.\nPlease pass the following format: { \'polygon\' : [(0, 0), (1, 0), (1, 1), (0, 1)] }'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8001)
